2cde2f09deb52c626cde20f0b7bcc8b42865d8a3
Will James (Laptop) <will.james@camotion.com>
2014-08-18 03:07:34 +1000

    First draft of source code complete.

--- core.cxx
+++ core.cxx
@@ -1,4 +1,5 @@
 #include "core.h"
+#include <sha.h>
 
 using pbpdp;
 
@@ -7,7 +8,16 @@ void scheme_parameters::init(const char *param)
 	pairing_init_set_str(_pairing,param);
 	element_init_G2(_g,_pairing);
 	element_random(_g);
+	_name_length = pairing_length_in_bytes_Zr(_pairing);
 	_sig_length = pairing_length_in_bytes_x_only_G1(_pairing);
+	_initialized = true;
+}
+
+void scheme_parameters::cleanup()
+{
+	element_clear(_g);
+	pairing_clear(_pairing);
+	_initialized = false;
 }
 
 void secret_parameters::init(const scheme_parameters &scheme)
@@ -18,6 +28,14 @@ void secret_parameters::init(const scheme_parameters &scheme)
 	// for PDP scheme
 	element_init_Zr(_x,scheme.get_pairing());
 	element_random(_x);
+	_initialized = true;
+}
+
+void secret_parameters::cleanup()
+{
+	element_clear(_ssk);
+	element_clear(_x);
+	_initialized = false;
 }
 
 void public_parameters::init(const scheme_parameters &scheme,const secret_parameters &sp)
@@ -33,30 +51,308 @@ void public_parameters::init(const scheme_parameters &scheme,const secret_parame
 	
 	element_init_GT(_euv,scheme.get_pairing());
 	element_pairing(_euv,_u,_v);
+	initialized = true;
 }
 
-void verification_metadata::generate_authenticators(const secret_parameters &s, const public_parameters &p, const scheme_parameters &scheme, file &f)
+void public_parameters::cleanup()
+{
+	element_clear(_spk);
+	element_clear(_u);
+	element_clear(_v);
+	element_clear(_euv);
+	_initialized = false;
+}
+
+void verification_metadata::init(const secret_parameters &s, const public_parameters &p, const scheme_parameters &scheme, file &f)
 {
 	unsigned int count = file.get_chunk_count();
-	allocate_authenticators(count);
+	allocate_authenticators(count,scheme);
+	element_t t0;
+	element_t t1;
+	element_t z0;
+	element_init_G1(t0);
+	element_init_G1(t1);
+	element_init_Zr(z0);
+	
+	element_t name;
+	element_init_Zr(name,scheme.get_pairing());
+	element_random(name);
+	
+	_name_len = scheme.get_name_len();
+	_name = new char[_name_len];
+	element_to_bytes_x_only(_name,name);
+	
+	element_clear(name);
+	
+	element_pp_t pp;
+	element_pp_init(pp,p.get_u());
+	
+	// calculate each sigma_i = (H(W_i)*u^m_i)^x
 	for (int i=0;i<count;i++)
 	{
-		_authenticators[i]
+		get_HW(t0,i);
+		
+		file.get_chunk(z0,i);
+		element_pp_pow_zn(t1,z0,pp);
+		
+		element_mul(t0,t0,t1);
+		element_pow_zn(_authenticators[i],t0,s.get_x());
 	}
+	
+	element_pp_clear(pp);
+	
+	// generate name signature
+	element_t name_sig;
+	element_init_G1(name_sig,scheme.get_pairing());
+	
+	// signature is name^ssk
+	element_from_hash(t0,_name,_name_len);
+	element_pow_zn(name_sig,t0,s.get_ssk());
+	
+	_name_sig_len = scheme.get_sig_len();
+	_name_sig = new char[_name_sig_len];
+	element_to_bytes_x_only(_name_sig,name_sig);
+	
+	element_clear(name_sig);
+	
+	element_clear(z0);
+	element_clear(t1);
+	element_clear(t0);
+	
+	_initialized = true;
 }
 
-void verification_metadata::allocate_authenticators(unsigned int count)
+void verification_metadata::cleanup()
+{
+	delete[] _name;
+	delete[] _name_sig;
+	_initialized = false;
+}
+
+void verification_metadata::allocate_authenticators(unsigned int count,const scheme_parameters &scheme)
 {
 	clear_authenticators();
-	_authenticators = new element_ptr[count];
+	_authenticators = new element_t[count];
+	for (int i=0;i<count;i++)
+	{
+		element_init_G1(_authenticators[i],scheme.get_pairing());
+	}
 	_count = count;
 }
+
 void verification_metadata::clear_authenticators()
 {
 	if (_count > 0)
 	{
+		for (int i=0;i<count;i++)
+		{
+			element_clear(_authenticators[i]);
+		}
 		delete[] _authenticators;
 		_count = 0;
+	}
+}
+
+void verification_metadata::check_sig(const public_parameters &p,const scheme_parameters &scheme) const
+{
+	// now we know sig = name^ssk and spk = g^ssk  we need to verify that e(sig,g) = e(name,spk)
+	
+	element_t name;
+	element_t name_sig;
+	element_init_G1(name,scheme.get_pairing());
+	element_init_G1(name_sig,scheme.get_pairing());
+	
+	element_from_x_bytes_only(name,_name);
+	element_from_x_bytes_only(name_sig,_name_sig);
+	
+	element_t p0,p1;
+	
+	element_init_GT(p0,scheme.get_pairing());
+	element_init_GT(p1,scheme.get_pairing());
+	
+	element_pairing(p0,name_sig,scheme.get_g());
+	element_pairing(p1,name,scheme.get_spk());
+	
+	// now we must compare the pairings, although since we only got the x coordinate,
+	// each one could be the inverse of the true value.
+	// so either p0 == p1 or 1/p0 == p1 (p0*p1 == 1)
+	bool sig_valid = false;
+	
+	if (!element_cmp(p0,p1))
+	{
+		sig_valid = true;
+	}
+	else
+	{
+		element_mul(p0,p0,p1);
+		if (!element_is1(p0))
+		{
+			sig_valid = true;
+		}
+	}
+	
+	element_clear(p1);
+	element_clear(p0);
+	
+	element_clear(name_sig);
+	element_clear(name);
+	
+	return sig_valid;
+}
+
+void verification_metadata::get_W_size() const
+{
+	return _name_len + sizeof(unsigned int);
+}
+
+void verification_metadata::get_Wi(unsigned char *W, unsigned int i) const
+{
+	memcpy(W,_name,_name_len);
+	memcpy(W+_name_len,(unsigned char*)&i,sizeof(unsigned int));
+}
+
+void verification_metadata::get_HWi(element_t e,unsigned int i) const
+{
+	static SHA256 sha256;
+	static unsigned char *W = 0;
+	static unsigned int W_cap = 0;
+	static unsigned char *HW = 0;
+	static unsigned int HW_cap = 0;
+	
+	if (W_cap < get_W_size())
+	{
+		if (W)
+		{
+			delete[] W;
+		}
+		W_cap = get_W_size();
+		W = new char[get_W_size()];
+	}
+	if (HW < sha256.DigestSize())
+	{
+		if (HW)
+		{
+			delete[] HW;
+		}
+		HW_cap = sha256.DigestSize();
+		HW = new char[sha256.DigestSize()];
+	}
+	
+	get_W(W,i);
+	sha256.CalculateDigest(HW,W,get_W_size());
+	
+	element_from_hash(e,HW,sha256.DigestSize());
+}
+
+void challenge::init(const scheme_parameters &scheme, unsigned int c, unsigned int chunk_count)
+{
+	if (c > 0)
+	{
+		_pairs = new pair[c];
+		_count = c;
+		
+		mpz_t mpz_s;
+		mpz_t mpz_lim;
+		
+		mpz_init(mpz_s);
+		
+		mpz_init_set_ui(mpz_lim,chunk_count);
+			
+		for (int i=0;i<_count;i++)
+		{
+			// select a random element
+			pbc_mpz_random(mpz_s,mpz_lim);
+			
+			_pairs[i]._s = mpz_get_ui(mpz_s);
+		
+			// select a random challenge value
+			element_init(_pairs[i]._v,scheme.get_pairing());
+			element_random(_pairs[i]._v);
+		}
+		
+		mpz_clear(mpz_lim);
+		mpz_clear(mpz_s);
+		
+		_initialized = true;
+	}
+}
+
+void challenge:cleanup()
+{
+	if (_initialized)
+	{
+		delete[] _pairs;
+		_count = 0;
+	}
+}
+
+void response_proof::init(const challenge &c, const verification_metadata &vm, const public_parameters &p,const scheme_parameters &scheme, file &f)
+{
+	element_t r;
+	element_t chunk;
+	element_t mu_prime;
+	element_t gamma;
+	element_t t0;
+	
+	element_init_Zr(r,scheme.get_pairing());
+	
+	element_random(r);
+	
+	element_init_GT(_R,scheme.get_pairing());
+	
+	element_pairing(_R,p.get_u(),p.get_v());
+	
+	// now R = e(u,v)^r, where r is random
+	element_pow_Zn(_R,_R,r);
+	
+	element_init_Zn(chunk);
+	element_init_Zn(mu_prime);
+	element_init_G1(_sigma);
+	element_init_G1(t0);
+	
+	element_set0(mu_prime);
+	element_set1(_sigma);
+	
+	// calculate the linear combination of sampled blocks from the challenge
+	// mu' = sum(v_i*mu_i)
+	for (int i=0;i<c.get_count();i++)
+	{
+		challenge::pair pair = c.get_pair(i);
+		file.get_chunk(chunk,pair._s);
+		
+		element_mul(chunk,pair._v,chunk);
+		element_add(mu_prime,mu_prime,chunk);
+		
+		element_pow_zn(t0,vm.get_authenticator(i),pair._v);
+		element_mul(_sigma,_sigma,t0);
+	}
+	
+	element_init_Zr(gamma,scheme.get_pairing());
+	
+	element_init_Zr(_mu,scheme.get_pairing());
+	
+	hash_element_to_element(gamma,R);
+	
+	// mu = r + gamma * mu'
+	element_mul(gamma,gamma,mu_prime);
+	element_add(_mu,r,gamma);
+	
+	element_clear(gamma);
+	element_clear(t0);
+	element_clear(mu_prime);
+	element_clear(chunk);
+	element_clear(r);
+	
+	_initialized = true;
+}
+
+void response_proof::cleanup()
+{
+	if (_initialized)
+	{
+		element_clear(_R);
+		element_clear(_sigma);
+		element_clear(_mu);
 	}
 }
 
@@ -67,21 +363,73 @@ void key_gen(scheme_parameters &scheme, secret_parameters &s, public_parameters 
 	p.init(scheme,s);
 }
 
-verification_metadata& sig_gen(const secret_parameters &s, const public_parameters &p, file &f)
+void sig_gen(verification_metadata &vmd,const secret_parameters &s, const public_parameters &p, file &f)
 {
+	vmd.init(s,p,scheme,f);
+}
+
+bool check_sig(const verification_metdata &vmd, const public_parameters &p, const scheme_parameters &scheme)
+{
+	vmd.check_sig(p,scheme);
+}
+
+void gen_challenge(challenge &chal, const scheme_parameters &scheme, unsigned int c, unsigned int chunk_count)
+{
+	// generates a challenge for c chunks of the file which has chunk count chunk_count
+	chal.init(scheme,c,chunk_count);
+}
+
+void gen_proof(response_proof& rp, const challenge &c, const verification_metadata &vm, const public_parameters &p,const scheme_parameters &scheme, file &f)
+{
+	rp.init(c,vm,p,scheme,file);
+}
+
+bool verify_proof(const response_proof &r, const challenge &c, const verification_metadata &vm, const public_parameters &p,const scheme_parameters &scheme)
+{
+	element_t gamma;
+	element_t lhs;
+	element_t rhs;
+	element_t t0;
+	element_t t1;
 	
-}
-
-challenge& gen_challenge(const public_parameters &p, unsigned int chunk_count)
-{
-}
-
-response_proof& gen_proof(const challenge &c, const verification_metadata &vm, const public_parameters &p)
-{
-}
-
-bool verify_proof(const response_proof &r, const challenge &c, const public_parameters &p)
-{
+	element_init_Zr(gamma,scheme.get_pairing());
+	
+	hash_element_to_elemen(gamma,r.get_R());
+	
+	element_init_G1(t0);
+	element_init_GT(lhs);
+	element_init_GT(rhs);
+	
+	element_pow_zn(t0,r.get_sigma(),gamma);
+	element_pairing(lhs,t0,scheme.get_g());
+	element_mul(lhs,R,lhs);
+	
+	element_init_G1(t1);
+	
+	element_set1(t1);
+	
+	for (int i=0;i<c.get_count();i++)
+	{
+		challenge::pair pair = c.get_pair(i);
+		vm.get_HWi(t0,pair._s);
+		element_pow_zn(t0,t0,pair._v);
+		
+		element_mul(t1,t1,t0);
+	}
+	
+	element_pow_zn(t1,t1,gamma);
+	
+	element_pow_zn(t0,p.get_u(),r.get_mu());
+	
+	element_mul(t0,t1,t0);
+	
+	element_pairing(rhs,t0,p.get_v());
+	
+	// check that they are equal.  for now all of the elements should have been 
+	// fully stored so they should always be equal.  in the future i may have to 
+	// serialize one coordinate of some elements for transport, which will mean
+	// that they may be inverses of each other
+	return !element_cmp(lhs,rhs);
 }
 
 void hash_data_to_element(element_t e,unsigned char *data,unsigned int len)
@@ -91,11 +439,15 @@ void hash_data_to_element(element_t e,unsigned char *data,unsigned int len)
 
 void hash_element_to_element(element_t out, element_t in)
 {
-	static unsigned char *buffer;
+	static unsigned char *buffer = 0;
 	static unsigned int cap = 0;
 	unsigned int req_len = element_length_in_bytes(in);
 	if (req_len > cap)
 	{
+		if (buffer)
+		{
+			delete[] buffer;
+		}
 		cap = req_len;
 		buffer = new char[cap];
 	}
--- core.h
+++ core.h
@@ -9,75 +9,100 @@ namespace pbpdp
 	class scheme_parameters
 	{
 	public:
-		scheme_parameters(const char *param) { init(param); }
 		void init(const char *param);
+		void cleanup();
 	
-		pairing_t get_pairing() { return _pairing; }
-		element_t get_g() { return _g; }
+		pairing_t get_pairing() const { return _pairing; }
+		element_t get_g() const { return _g; }
+		unsigned int get_name_len() const { return _name_length; }
+		unsigned int get_sig_len() const { return _sig_length; }
 		
 	private:
+		bool				_initialized;
 		pairing_t 			_pairing;
-		element_t			_g;
+		element_t			_g;					// G2
+		unsigned int 		_name_length;
 		unsigned int		_sig_length;
 	};
 	
 	class secret_parameters
 	{
 	public:
-		secret_parameters(const scheme_parameters &scheme) { init(scheme); }
 		void init(const scheme_parameters &scheme);
+		void cleanup();
 		
-		element_t get_ssk() { return _ssk; }
-		element_t get_x() { return _x; }
+		element_t get_ssk() const { return _ssk; }
+		element_t get_x() const { return _x; }
 		
 	private:
-		element_t			_ssk;
-		element_t 			_x;
+		bool				_initialized;
+		element_t			_ssk;				// Zp
+		element_t 			_x;					// Zp
 	};
 
 	class public_parameters
 	{
 	public:
-		public_parameters(const scheme_parameters &scheme,const secret_parameters &sp) { init(sp); }
-		void init(const secret_parameters &sp);
+		void init(const scheme_parameters &scheme, const secret_parameters &sp);
+		void cleanup();
 		
-		element_t get_spk() { return _spk; }
-		element_t get_u() { return _u; }
-		element_t get_v() { return _v; }
-		element_t get_pair() { return _euv; }
+		element_t get_spk() const { return _spk; }
+		element_t get_u() const { return _u; }
+		element_t get_v() const { return _v; }
+		element_t get_pair() const { return _euv; }
 		
 	private:
-		element_t			_spk;
-		element_t			_u;
-		element_t			_v;
-		element_t			_euv;
+		bool 				_initialized;
+		element_t			_spk;			// G2
+		element_t			_u;				// G1
+		element_t			_v;				// G2
+		element_t			_euv;			// GT
 	};
 
 	class verification_metadata
 	{
 	public:
-		void generate_authenticators(const secret_parameters &s, const public_parameters &p, const scheme_parameters &scheme, file &f);
+		void init(const secret_parameters &s, const public_parameters &p, const scheme_parameters &scheme, file &f);
+		void cleanup();
+		
 		void allocate_authenticators(unsigned int count);
 		void clear_authenticators();
 		
+		bool check_sig(const public_parameters &p,const scheme_parameters &scheme) const;
+		
+		element_t get_authenticator(unsigned int i);
+		
+		void verification_metadata::get_W_size() const;
+		void get_Wi(unsigned char *W, unsigned int sz, unsigned int i) const;
+		void get_HWi(element_t e,unsigned int i) const;
+		
 	private:
+		bool				_initialized;
 		element_t* 			_authenticators;
 		unsigned int 		_count;
-		element_t 			_name;
+		unsigned char *		_name;
+		unsigned int		_name_len;
 		unsigned char *		_name_sig;
+		unsigned int		_name_sig_len;
 	};
-
-	typedef struct 
-	{
-		unsigned int		_i;
-		element_t			_v;
-	} challenge_pair;
 	
 	class challenge
 	{
 	public:
+		typedef struct 
+		{
+			unsigned int		_s;
+			element_t			_v;
+		} pair;
 	
+		void init(const scheme_parameters &scheme,unsigned int c, unsigned int chunk_count);
+		void cleanup();
+		
+		unsigned int get_count() const { return _count; }
+		pair get_pair(unsigned int i) const { return _pairs[i]; }
+		
 	private:
+		bool				_initialized;
 		challenge_pair *	_pairs;
 		unsigned int 		_count;
 	};
@@ -85,11 +110,18 @@ namespace pbpdp
 	class response_proof
 	{
 	public:
+		void init(const challenge &c, const verification_metadata &vm, const public_parameters &p,const scheme_parameters &scheme, file &f);
+		void cleanup();
+		
+		element_t get_mu() const { return _mu; }
+		element_t get_sigma() const { return _sigma; }
+		element_t get_R() const { return _R; }
 		
 	private:
-		element_t			_mu;
-		element_t			_sigma;
-		element_t			_R;
+		bool				_initialized;
+		element_t			_mu;				// Zp
+		element_t			_sigma;				// G1
+		element_t			_R;					// GT
 	};
 	
 	class file
@@ -101,7 +133,8 @@ namespace pbpdp
 	
 	void key_gen(const scheme_parameters &scheme,secret_parameters &s, public_parameters &p);
 	void sig_gen(verification_metadata &vmd, const secret_parameters &s, const public_parameters &p, const scheme_parameters &scheme, file &f);
-	void gen_challenge(challenge &chal,const public_parameters &p, const scheme_parameters &scheme, unsigned int chunk_count);
+	bool check_sig(const verification_metadata &vmd, const public_parameters &p, const scheme_parameters &scheme);
+	void gen_challenge(challenge &chal,const public_parameters &p, const scheme_parameters &scheme, unsigned int c, unsigned int chunk_count);
 	void gen_proof(response_proof &rp, const challenge &c, const verification_metadata &vm, const public_parameters &p, const scheme_parameters &scheme);
 	bool verify_proof(const response_proof &r, const challenge &c, const public_parameters &p);
 	
